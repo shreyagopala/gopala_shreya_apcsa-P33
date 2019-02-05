ee67da50dad89fee95d5eba5fafce7fdc53a4500
shreyagopala <shreya.gopala@gmail.com>
2019-02-04 20:53:21 -0800

    unit02 assignments

---
+++ Unit02/src/Line.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		double m = 0.00;
+		m = (double)(y2-y1)/(x2-x1);
+		return m;
+	}
+
+}
---
+++ Unit02/src/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+	double calc = (double)1/2*(base1 + base2)*height;
+	return calc;
+	}
+}
