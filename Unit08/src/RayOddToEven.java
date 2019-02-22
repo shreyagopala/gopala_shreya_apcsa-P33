a873ad216dde17cd2892296b36e880dd1a3ce5e7
shreyagopala <shreya.gopala@gmail.com>
2019-02-21 21:15:20 -0800

    unit08 work

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,49 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public int go(int[] ray)
 	{
-		return false;
+		int odd = 0;
+		int even = 0;
+		int oddIndex = 0;
+		int evenIndex = 0;
+		boolean foundOdd = false;
+		boolean foundEven = false;
+		
+		if (ray.length == 0) return -1;
+		//first lets find the odd number
+		for (int i = 0; i < ray.length; i++) 
+		{
+			if (ray[i]%2 == 0) 
+			{
+				//this means the number is even
+				foundEven = true;
+			}
+			else //this means the number is odd
+			{
+				odd = ray[i];
+				oddIndex = i;
+				foundOdd = true;
+				break;
+			}
+		}
+		//reset the boolean for even since we have to find next even
+		foundEven = false;
+		//now lets try to find the next even
+		for (int j = oddIndex; j < ray.length; j++) 
+		{
+			if (ray[j]%2 == 0) 
+			{
+				even = ray[j];
+				evenIndex = j;
+				foundEven = true;
+				break;
+			}
+		}
+	
+		//Return -1 if no odd numbers are found or there are no even numbers following an odd number
+		if (!foundOdd || !foundEven) return -1;	
+		//Return the distance between the first odd number and the FIRST even number
+		return (evenIndex - oddIndex);
 	}
 }
