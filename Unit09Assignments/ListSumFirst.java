1e36fdd73adbe675af8c8f57c0cb3e240cef4958
shreyagopala <shreya.gopala@gmail.com>
2019-02-26 21:23:26 -0800

    unit09 start

---
+++ Unit09Assignments/ListSumFirst.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSumFirst
+{
+	public static int go(List<Integer> ray)
+	{
+		int sum = 0;
+		int first = ray.get(0);
+	if (ray.size() > 0) {
+		for (int i = 1; i < ray.size(); i++)
+		{
+			if (ray.get(i) > first) sum += ray.get(i)
+		}
+		if (sum == 0) return -1;
+		else return sum;
+	}
+	return -1;
+	}
+}
---
+++ Unit09Assignments/SumFirstRunner.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class SumFirstRunner
+{
+	public static void main( String args[] )
+	{	
+		ListSumFirst test = new ListSumFirst();
+		
+	}
+}
