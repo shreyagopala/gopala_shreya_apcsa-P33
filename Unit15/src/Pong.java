bab28fc6a98d6895567ce0dc1bde1e3a69930175
shreyagopala <shreya.gopala@gmail.com>
2019-04-19 12:08:42 -0700

    fixed mistakes in pong

--- Unit15/src/Pong.java
+++ Unit15/src/Pong.java
@@ -16,27 +16,31 @@ import java.awt.event.ActionListener;
 public class Pong extends Canvas implements KeyListener, Runnable
 {
 	private Ball ball;
-	private Paddle leftPaddle;
-	private Paddle rightPaddle;
+	private Paddle leftPaddle, rightPaddle;
 	private boolean[] keys;
 	private BufferedImage back;
-
-
+	private int scoreRight, scoreLeft;
+	private Wall rightWall, leftWall, topWall, botWall;
+	
 	public Pong()
 	{
 		//set up all variables related to the game
-
-
-
-
+		ball = new BlinkyBall();
+		leftPaddle = new Paddle(20, 10, 10, 40, Color.RED, 5);
+		rightPaddle = new Paddle(770, 10, 10, 40, Color.RED, 5);
 		keys = new boolean[4];
-
+		scoreRight = 0;
+		scoreLeft = 0;
+		rightWall = new Wall(790, 0, 10, 600, Color.WHITE);
+		leftWall = new Wall(0, 0, 10, 600, Color.WHITE);
+		topWall = new Wall(0, 0, 800, 10, Color.WHITE);
+		botWall = new Wall(0, 490, 800, 10, Color.WHITE);
     
     	setBackground(Color.WHITE);
-		setVisible(true);
+		setVisible(true); //generates background thread that calls update()
 		
-		new Thread(this).start();
-		addKeyListener(this);		//starts the key thread to log key strokes
+		this.addKeyListener(this);		//starts the key thread to log key strokes
+		new Thread(this).start();	
 	}
 	
    public void update(Graphics window){
@@ -57,52 +61,122 @@ public class Pong extends Canvas implements KeyListener, Runnable
 		//we will draw all changes on the background image
 		Graphics graphToBack = back.createGraphics();
 
-
+		graphToBack.setColor(Color.RED);
+		
 		ball.moveAndDraw(graphToBack);
 		leftPaddle.draw(graphToBack);
 		rightPaddle.draw(graphToBack);
-
-
-		//see if ball hits left wall or right wall
-		if(!(ball.getX()>=10 && ball.getX()<=780))
+		
+				//see if ball hits left wall or right wall
+		//if(!(ball.getX()>=10 && ball.getX()<=780))
+		if(ball.getX() <= leftWall.getX() + leftWall.getWidth())
 		{
+			if(ball.getXSpeed() != 0) scoreRight++;
 			ball.setXSpeed(0);
 			ball.setYSpeed(0);
+			ball.draw(graphToBack, Color.WHITE);
+			
+			try
+			{
+				Thread.currentThread().sleep(950);
+			}catch(Exception e){ }
+			//set position and speed
+			ball.setX((int)(Math.random() * 400));
+			ball.setY((int)(Math.random() * 400));
+			ball.setXSpeed(2);
+			ball.setYSpeed(1);	
+		}
+		
+		if(ball.getX() + ball.getWidth() >= rightWall.getX())
+		{	
+			if(ball.getXSpeed() != 0) scoreLeft++;
+			
+			ball.setXSpeed(0);
+			ball.setYSpeed(0);
+			ball.draw(graphToBack, Color.WHITE);
+			
+			try
+			{
+				Thread.currentThread().sleep(950);
+			}catch(Exception e){ }
+			//set position and speed
+			ball.setX((int)(Math.random() * 400));
+			ball.setY((int)(Math.random() * 400));
+			ball.setXSpeed(2);
+			ball.setYSpeed(1);	
+		}
+		
+		//make a rectangle and paint it white every time so we don't have weird leftover scores
+		graphToBack.setColor(Color.WHITE);
+		graphToBack.fillRect(440,  520,  80,  80);
+		graphToBack.fillRect(640,  520, 100, 100);
+		graphToBack.fillRect(140, 520, 100, 100);
+		
+		graphToBack.setColor(Color.RED);
+		
+		graphToBack.drawString("Right Player: " + scoreRight, 400, 540);
+		graphToBack.drawString("Left Player: " + scoreLeft, 400, 560);
+	
+		//see if the ball hits the top or bottom wall 
+		if(ball.getY() + ball.getHeight() >= botWall.getY() || ball.getY() <= topWall.getY())
+		{
+			ball.setYSpeed(-ball.getYSpeed());
+			ball.setColor(((BlinkyBall)ball).randomColor());
 		}
 
-		
-		//see if the ball hits the top or bottom wall 
-
-
-
-
 		//see if the ball hits the left paddle
-		
-		
-		
-		//see if the ball hits the right paddle
-		
-		
-		
+		//why do you add ball x Spd??
+		if(ball.getX() <= leftPaddle.getX() + leftPaddle.getWidth() + Math.abs(ball.getXSpeed())
+				&& (ball.getY() >= leftPaddle.getY() 
+					&& ball.getY() <= leftPaddle.getY() + leftPaddle.getHeight()
+						|| ball.getY() + ball.getHeight() >= leftPaddle.getY() 
+						&& ball.getY() + ball.getHeight() < leftPaddle.getY() + leftPaddle.getHeight()))
+		{
+			if(ball.getX() <= leftPaddle.getX() + leftPaddle.getWidth() - Math.abs(ball.getXSpeed()))
+				{
+					(ball).setYSpeed(-ball.getYSpeed());
+					if(ball.getXSpeed() != 0) ball.setColor(((BlinkyBall)ball).randomColor());
+				}
+			else 
+			{
+				ball.setXSpeed(-ball.getXSpeed());
+				if(ball.getYSpeed() != 0) ball.setColor(((BlinkyBall)ball).randomColor());
+			}			
+		}
+			
+		//see if the ball hits the right paddle; still probably buggy
+		if(ball.getX() >= rightPaddle.getX() - rightPaddle.getWidth() + Math.abs(ball.getXSpeed())
+				&& (ball.getY() >= rightPaddle.getY() 
+					&& ball.getY() <= rightPaddle.getY() + rightPaddle.getHeight()
+						|| ball.getY() + ball.getHeight() >= rightPaddle.getY()
+						&& ball.getY() + ball.getHeight() < rightPaddle.getY() + rightPaddle.getHeight()))
+		{
+			//what does this if statement even do?? it's probably incorrect
+			if(ball.getX() >= rightPaddle.getX() - Math.abs(ball.getXSpeed()))
+			{
+				ball.setYSpeed(-ball.getYSpeed());
+				if(ball.getXSpeed() != 0) ball.setColor(((BlinkyBall)ball).randomColor());
+			}
+			else
+			{
+				System.out.println(ball.getXSpeed());
+				ball.setXSpeed(-ball.getXSpeed());
+				System.out.println(ball.getXSpeed());
+				if(ball.getYSpeed() != 0) ball.setColor(((BlinkyBall)ball).randomColor());
 
-
+			}
+		}
+		
 		//see if the paddles need to be moved
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		if(keys[0]) leftPaddle.moveUpAndDraw(graphToBack); //need to use graphToBack instead of window 
+		if(keys[1]) leftPaddle.moveDownAndDraw(graphToBack);
+		if(keys[2]) rightPaddle.moveUpAndDraw(graphToBack);
+		if(keys[3]) rightPaddle.moveDownAndDraw(graphToBack);
 		
+		//draw scoring--IT'S NOT WORKING
+		graphToBack.setColor(Color.RED);
+		graphToBack.drawString("Left Player Score: " + scoreLeft + "\nRight Player Score: " + scoreRight, 100, 600);
+
 		twoDGraph.drawImage(back, null, 0, 0);
 	}
 
